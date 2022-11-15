667b20b2e31f475f8eba66874fccc55cfadcd23e
Martin Denis <denis.martin355@gmail.com>
2022-11-15 17:06:52 +0200

    Red UnitTest

--- ASP.NET Fundamentals/RestaurantTest/UnitTest1.cs
+++ ASP.NET Fundamentals/RestaurantTest/UnitTest1.cs
@@ -23,7 +23,7 @@ namespace RestaurantTest
 
             foreach (var restaurant in restaurants)
             {
-                //Assert.Equal("z", restaurant.Name);
+                Assert.Equal("z", restaurant.Name);
             }
         }
     }
