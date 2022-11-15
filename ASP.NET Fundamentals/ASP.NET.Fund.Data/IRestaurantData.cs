64b0a06a09cb558cf01cd2c62c90570a993d5c7a
Martin Denis <denis.martin355@gmail.com>
2022-11-15 17:14:57 +0200

    build err

--- ASP.NET Fundamentals/ASP.NET.Fund.Data/IRestaurantData.cs
+++ ASP.NET Fundamentals/ASP.NET.Fund.Data/IRestaurantData.cs
@@ -17,7 +17,7 @@ namespace ASP.NET.Fund.Data
         readonly List<Restaurant> restaurants;
         public InMemoryRestaurantData()
         {
-            restaurants = new List<Restaurant>()
+            restaurants = new Listqwqwq<Restaurant>()
             {
                 new Restaurant {Id = 1, Name = "La Costa", Location="London", Cuisine=CuisineType.Mexican },
                 new Restaurant {Id = 2, Name = "Scott Pizza", Location="California", Cuisine=CuisineType.Italian},
